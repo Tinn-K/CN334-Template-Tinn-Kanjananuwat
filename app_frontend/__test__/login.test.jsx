25541525e6b27ad6c1e8ef2da47d61f9b88b45ff
Tinn-K <tinn.kanjananuwat@gmail.com>
2024-04-24 04:45:40 +0700

    CN334(66)-W14-InClass

---
+++ app_frontend/__test__/login.test.jsx
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import Login from "../pages/login/index";
+
+describe("Login", () => {
+  it("renders inputs", () => {
+    const { container } = render(<Login />);
+    const username = container.querySelector(`input[name="username"]`);
+    const password = container.querySelector(`input[name="password"]`);
+    expect(username).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+  });
+});
